97abf7b96d4a7c93928fcd825d43f2cb90678ec8
yagocam <yagocam5@gmail.com>
2021-05-07 11:30:32 -0300

    mapa atualizado

--- scripts/GlobalMissionScript.gd
+++ scripts/GlobalMissionScript.gd
@@ -2,3 +2,4 @@ extends Node
 var goblinsKilled = 0 
 var slimesKilled = 0
 var skeletonsKilled = 0
+
--- scripts/HUD_Quest_Script.gd
+++ scripts/HUD_Quest_Script.gd
@@ -2,7 +2,7 @@ extends CanvasLayer
 var title = "Titulo"
 var mission = "Fale com o taverneiro"
 export var missionCount = 0
-export var missionLimit = 15
+export var missionLimit = 5
 
 func _ready():
 	$titleQuest.text = title
